049b54e02bcf97414a158a4cd8c203f2348fb661
K D <danila.krn@gmail.com>
2023-03-14 16:42:51 +0300

    forth example and .gitignore

--- examples/example3_gr12_KryvanosDanila.c
+++ examples/example3_gr12_KryvanosDanila.c
@@ -2,20 +2,11 @@
 #include <stdlib.h>
 #include <sqlite3.h> 
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
-   int i;
-   for(i = 0; i<argc; i++) {
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   printf("\n");
-   return 0;
-}
-
 int main(int argc, char* argv[]) {
    sqlite3 *db;
    char *zErrMsg = 0;
    int rc;
-   char *sql;
+   
 
    rc = sqlite3_open("test.db", &db);
    
@@ -26,17 +17,12 @@ int main(int argc, char* argv[]) {
       fprintf(stderr, "Opened database successfully\n");
    }
 
-   /* Create SQL statement */
-   sql = "INSERT INTO Patient (ID,NAME,SURNAME,AGE,DEPARTMENT,DIAGNOSIS) "  \
-         "VALUES (1, 'John', 'Doe', 35, 'Cardiology', 'Myocardial Infarction'); " \
-         "INSERT INTO COMPANY (ID,NAME,SURNAME,AGE,DEPARTMENT,DIAGNOSIS) "  \
-         "VALUES (2, 'Jane', 'Smith', 42, 'Oncology', 'Breast Cancer'); "     \
-         "INSERT INTO COMPANY (ID,NAME,SURNAME,AGE,DEPARTMENT,DIAGNOSIS)" \
-         "VALUES (3, 'James', 'Johnson', 26, 'Pediatrics', 'Influenza');" \
-         "INSERT INTO COMPANY (ID,NAME,SURNAME,AGE,DEPARTMENT,DIAGNOSIS)" \
-         "VALUES (4, 'Emily', 'Davis', 51, 'Gastroenterology', 'Gastritis');";
+   char *sql = "INSERT INTO Patient VALUES(1, 'John', 'Doe', 35, 'Cardiology', 'Myocardial Infarction');"
+               "INSERT INTO Patient VALUES(2, 'Jane', 'Smith', 42, 'Oncology', 'Breast Cancer');"
+               "INSERT INTO Patient VALUES(3, 'James', 'Johnson', 26, 'Pediatrics', 'Influenza');"
+               "INSERT INTO Patient VALUES(4, 'Emily', 'Davis', 51, 'Gastroenterology', 'Gastritis');";
 
-   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+   rc = sqlite3_exec(db, sql, 0, 0, &zErrMsg);
    
    if( rc != SQLITE_OK ){
       fprintf(stderr, "SQL error: %s\n", zErrMsg);
