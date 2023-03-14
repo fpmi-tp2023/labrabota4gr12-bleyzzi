684884b00f057812c7d9e35bf25b9aaad7771cf2
K D <danila.krn@gmail.com>
2023-03-14 10:42:51 +0300

    first example

--- examples/у example1_gr12_KryvanosDanila.c
+++ examples/у example1_gr12_KryvanosDanila.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <sqlite3.h>
 
-int main()
+int main(int argc, char* argv[])
 {
+    sqlite3 *db;
+    char *errmsg = 0;
+    int rc;
+
+    rc = sqlite3_open("test.db", &db);
+    if(rc)
+    {
+        fprintf(stderr, "Cant open db: %s\n", sqlite3_errmsg(db));
+        return 0;
+    }
+    else
+    {
+        fprintf(errmsg, 'Opened db successfully\n');
+    }
+    sqlite3_close(db);
     return 0;
 }
