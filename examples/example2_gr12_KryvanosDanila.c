a4b19229f051f5013dbb8e6300ce6387adc4e0b7
K D <danila.krn@gmail.com>
2023-03-15 08:47:07 +0300

    minor fix

--- examples/example2_gr12_KryvanosDanila.c
+++ examples/example2_gr12_KryvanosDanila.c
@@ -1,16 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h> 
-
-static int callback(void *NotUsed, int argc, char **argv, char **azColName) {
-   int i;
-   for(i = 0; i<argc; i++) {
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   printf("\n");
-   return 0;
-}
-
 int main(int argc, char* argv[]) {
    sqlite3 *db;
    char *zErrMsg = 0;
@@ -34,7 +24,7 @@ int main(int argc, char* argv[]) {
       "DEPARTMENT        TEXT," \
       "DIAGNOSIS         TEXT );";
 
-   rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+   rc = sqlite3_exec(db, sql, 0, 0, &zErrMsg);
    
    if( rc != SQLITE_OK ){
       fprintf(stderr, "SQL error: %s\n", zErrMsg);
